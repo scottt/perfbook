3c89df9be01da35db7993b3b857e4fe20c11f30b
Akira Yokosawa <akiysw@gmail.com>
2016-04-24 09:02:50 +0900

    Improve behavior of build scripts
    This commit takes care of the possible increase of pdflatex
    iterations introduced by the previous commit ("qqz: Improve
    accuracy of cross-links") that would cause 'make' to terminate
    prematurely. The same issue seems to have been observed since
    commit 33b93f8258f5 ("qqz: Cross-link questions and answers").
    However, no one has attempted to fix it because a final PDF can
    be obtained by just repeating 'make' again.
    
    To fix the issue, this commit refactors 'runlinux.sh' into 3 parts.
    
    1) bibtex related part is moved to 'Makefile' as rules for targets
    such as 'perfbook.bbl'. It also adds necessary rules which support
    the '.bbl' rules.
    
    2) 'runfirstlatex.sh' is separated so that it can be invoked in
    rules for '.aux' targets which support '.bbl' rules.
    
    3) 'runlatex.sh' is intensively modified. The first while loop
    watches the 'LaTeX Warning: There were undefined references'
    message and the second one watches the 'LaTeX Warning: Label(s)
    may have changed' message.
    The loops watch the variation in 'LaTeX Warning:" lines in
    $basename.log and when they find no change, they will give up.
    Note that in early rounds of the loop, there may be cases when
    there is no change in 'LaTeX Warning' messages while there
    is still room for improvement. To accommodate this, giving-up
    will fire only after a certain number of iterations for each loop.
    Since the minimum iteration numbers are chosen after trial and
    error, they might need to be changed in the future.
    
    For each round of the loop, a message representing which warning
    is being watched is displayed.
    When the giving-up logic kicks in, grep of the remaining warnings
    is also displayed.
    
    '$basename-first.log', '$basename-warning.log' and
    '$basename-warning-prev.log' files are used to track things.
    They are removed when the build succeeds.
    
    This commit also updates the copyright notices in the scripts.
    
    Signed-off-by: Akira Yokosawa <akiysw@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- utilities/runlatex.sh
+++ utilities/runlatex.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 #
-# Run latex on the specified file and bibliography directory.
+# Run pdflatex on the specified file.
 # Attempt to avoid useless repeats.
+# This version is heavily customized to be used for perfbook.
+# It is assumed to be used together with runfirstlatex.sh
+# and Makefile of perfbook.
 #
-# Usage: runlatex.sh file.tex [ bibdir ]
+# Usage: sh runlatex.sh file[.tex]
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,51 +19,123 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program; if not, you can access it online at
+# http://www.gnu.org/licenses/gpl-2.0.html.
 #
 # Copyright (C) IBM Corporation, 2012
+# Copyright (C) Akira Yokosawa, 2016, 2017
 #
 # Authors: Paul E. McKenney <paulmck@us.ibm.com>
+#          Akira Yokosawa <akiyks@gmail.com>
+
+diff_warning () {
+	if diff -q $basename-warning.log $basename-warning-prev.log >/dev/null
+	then
+		echo "No more improvement is expected, giving up."
+		return 0 ;
+	else
+#		echo "Some improvements are observed, continuing."
+		return 1 ;
+	fi
+}
+
+identical_warnings () {
+	if test -r $basename-warning-prev.log
+	then
+		if test "$iter" -gt "$min_iter" && diff_warning
+		then
+			return 0 ;
+		fi
+	fi
+	return 1 ;
+}
+
+iterate_latex () {
+	pdflatex $basename > /dev/null 2>&1 < /dev/null || :
+	if grep -q '! Emergency stop.' $basename.log
+	then
+		grep -B 15 -A 5 '! Emergency stop.' $basename.log
+		echo "----- Fatal latex error, see $basename.log for details. -----"
+		exit 1
+	fi
+	if grep -q '!pdfTeX error:' $basename.log
+	then
+		grep -A 2 '!pdfTeX error:' $basename.log
+		echo "----- Fatal latex error, see $basename.log for details. -----"
+		exit 1
+	fi
+	if test -r $basename-warning.log
+	then
+		mv -f $basename-warning.log $basename-warning-prev.log
+	fi
+	grep 'LaTeX Warning:' $basename.log > $basename-warning.log
+	return 0 ;
+}
 
 if test -z "$1"
 then
-	echo No latex file, aborting.
+	echo No latex file specified, aborting.
 	exit 1
 fi
 
 basename=`echo $1 | sed -e 's/\.tex$//'`
 
-iter=1
-echo "pdflatex $iter"
-pdflatex $basename > /dev/null 2>&1 < /dev/null || :
-if grep -q '! Emergency stop.' $basename.log
+if ! test -r $basename-first.log
 then
-	echo "----- Fatal latex error, see $basename.log for details. -----"
-fi
-if grep -q 'LaTeX Warning: There were undefined references' $basename.log
-then
-	if test -d "$2"
+	if ! sh utilities/mpostcheck.sh
 	then
-		bibtex $basename || :
-	else
-		echo "No bibliography directory, skipping bibtex."
-	fi
-fi
-while grep -q 'LaTeX Warning: Label(s) may have changed. Rerun to get cross-references right.' $basename.log
-do
-	iter=`expr $iter + 1`
-	echo "pdflatex $iter"
-	pdflatex $basename > /dev/null 2>&1 < /dev/null || :
-	if grep -q '! Emergency stop.' $basename.log
-	then
-		echo "----- Fatal latex error, see $basename.log for details. -----"
-	fi
-	if test "$iter" -eq 4
-	then
-		echo "Iteration limit: $iter passes through pdflatex"
 		exit 1
 	fi
+	echo "pdflatex 1 for $basename.pdf"
+	iterate_latex
+fi
+rm -f $basename-first.log
+iter=2
+echo "pdflatex 2 for $basename.pdf # for possible bib update"
+iterate_latex
+min_iter=2
+while grep -q 'LaTeX Warning: There were undefined references' $basename.log
+do
+	if identical_warnings
+	then
+		break
+	fi
+	iter=`expr $iter + 1`
+	echo "pdflatex $iter for $basename.pdf # remaining undefined refs"
+	iterate_latex
 done
-grep "LaTeX Warning:" $basename.log
+min_iter=3
+while grep -q 'LaTeX Warning: Label(s) may have changed' $basename.log
+do
+	if identical_warnings
+	then
+		break
+	fi
+	iter=`expr $iter + 1`
+	echo "pdflatex $iter for $basename.pdf # label(s) may have changed"
+	iterate_latex
+done
+if grep -q "LaTeX Warning:" $basename.log
+then
+	echo "----- Excerpt around remaining warning messages -----"
+	grep -B 8 -A 5 "LaTeX Warning:" $basename.log | tee $basename-warning.log
+	echo "----- You can see $basename-warning.log for the warnings above. -----"
+	echo "----- If you need to, see $basename.log for details. -----"
+	rm -f $basename-warning-prev.log
+	exit 1
+fi
+rm -f $basename-warning.log $basename-warning-prev.log
+echo "'$basename.pdf' is ready."
+# to avoid redundant run of bibtex and pdflatex
+touch $basename.bbl
+touch $basename.pdf
+if ! strings cartoons/r-2014-Old-man-and-Brat.pdf | grep -q -i -e "Steel City Comic" -e "Test"
+then
+	echo "#######################################################################"
+	echo "## Steel City Comic font is not found in the resulting PDF!          ##"
+	echo "## Some speech balloons in the cartoons have been rendered awkwardly.##"
+	echo "## See item 1 in FAQ.txt and FAQ-BUILD.txt to fix the font issue.    ##"
+	echo "#######################################################################"
+fi
+sh utilities/mpostcheck.sh
 exit 0
